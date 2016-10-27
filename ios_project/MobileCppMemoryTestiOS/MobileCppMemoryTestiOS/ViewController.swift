c99719de299470d3c34aa3eb4c7a949e0feea81d
Bruno Coelho <4brunu@users.noreply.github.com>
2016-10-27 14:32:53 +0100

    remove global variable

--- ios_project/MobileCppMemoryTestiOS/MobileCppMemoryTestiOS/ViewController.swift
+++ ios_project/MobileCppMemoryTestiOS/MobileCppMemoryTestiOS/ViewController.swift
@@ -9,8 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController, MCMTFatherCallback {
-
-    var modelFather: MCMTModelFather?
     
     override func viewDidLoad() {
         super.viewDidLoad()
